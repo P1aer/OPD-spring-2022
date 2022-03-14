ec9190e7d74d46b8a6ae22468af31b142b93e3af
Ilya Osipov <arhoangel@bk.ru>
2022-03-14 16:40:11 +0300

    separate tests into 6 small ones

--- Matrix test/UnitTest1.cs
+++ Matrix test/UnitTest1.cs
@@ -6,53 +6,67 @@ namespace Matrix_test
     public class Tests
     {
         [Test]
-        public void MatrixSumTestSort()
+        public void MatrixSumTestSortAsc()
         {
             int[][] matrixSumAsc = new int[][] { new int[] { 5, 3, 1 }, new int[] { 5, 4, -3 }, new int[] { 0, -15, 20 }, new int[] { 2, 0, 1 }, new int[] { 2, 0, 0 } };
-            int[][] matrixSumDesc = new int[][] { new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 0, -15, 20 }, new int[] { 5, 4, -3 }, new int[] { 5, 3, 1 } };
+           
             Matrix matrix1 = new Matrix();
-            Matrix matrix2 = new Matrix();
-            matrix1.SortMatrix();
-            Assert.AreEqual(matrix1.matrix, matrix2.matrix);
             matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new SumType(true));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixSumAsc);
+        }
+        public void MatrixSumTestSortDesc()
+        {
+            int[][] matrixSumDesc = new int[][] { new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 0, -15, 20 }, new int[] { 5, 4, -3 }, new int[] { 5, 3, 1 } };
+            Matrix matrix1 = new Matrix();
+            matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new SumType(false));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixSumDesc);
         }
 
         [Test]
-        public void MatrixMaxTestSort()
+        public void MatrixMaxTestSortAsc()
         {
             int[][] matrixMaxAsc = new int[][] { new int[] { 0, -15, 20 }, new int[] { 5, 4, -3 }, new int[] { 5, 3, 1 }, new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 } };
-            int[][] matrixMaxDesc = new int[][] { new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 5, 4, -3 },new int[] { 5, 3, 1 } , new int[] { 0, -15, 20 } };
             Matrix matrix1 = new Matrix();
-            matrix1.SortMatrix();
             matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new MaxType(true));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixMaxAsc);
+        }
+        [Test]
+        public void MatrixMaxTestSortDesc()
+        {
+            int[][] matrixMaxDesc = new int[][] { new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 5, 4, -3 }, new int[] { 5, 3, 1 }, new int[] { 0, -15, 20 } };
+            Matrix matrix1 = new Matrix();
+            matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new MaxType(false));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixMaxDesc);
         }
         [Test]
-        public void MatrixMinTestSort()
-        {
-            int[][] matrixMinDesc = new int[][] { new int[] { 0, -15, 20 }, new int[] { 5, 4, -3 }, new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 5, 3, 1 } };
-            int[][] matrixMinAsc = new int[][] { new int[] { 5, 3, 1 }, new int[] { 2, 0, 0 },new int[] { 2, 0, 1 } , new int[] { 5, 4, -3 }, new int[] { 0, -15, 20 } };
+        public void MatrixMinTestSortAsc()
+        {    
+            int[][] matrixMinAsc = new int[][] { new int[] { 5, 3, 1 }, new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 5, 4, -3 }, new int[] { 0, -15, 20 } };
             Matrix matrix1 = new Matrix();
-            matrix1.SortMatrix();
             matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new MinType(true));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixMinAsc);
+        }
+        [Test]
+        public void MatrixMinTestSortDesc()
+        {
+            int[][] matrixMinDesc = new int[][] { new int[] { 0, -15, 20 }, new int[] { 5, 4, -3 }, new int[] { 2, 0, 0 }, new int[] { 2, 0, 1 }, new int[] { 5, 3, 1 } };           
+            Matrix matrix1 = new Matrix();
+            matrix1.setAlgorythm(new BubleSort());
             matrix1.setType(new MinType(false));
             matrix1.SortMatrix();
             Assert.AreEqual(matrix1.matrix, matrixMinDesc);
         }
+
         [Test]
         public void CountdownTest()
         {
